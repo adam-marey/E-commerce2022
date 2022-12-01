ef1b89a515746b0fec46fb404c091a5e04397192
adam <adammarey51@gmail.com>
2022-12-01 16:13:23 -0700

    completed single product details components in the slug

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 Adam's Products All rights reserved</p>
+      <p className="icons">
+        <AiFillGithub />
+        <AiFillLinkedin />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
